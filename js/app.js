64f0af55815627f3d0c9f701453af962adc8db8c
Pranta07 <palpranta@gmail.com>
2021-08-26 01:58:21 +0600

    JSON parse & stringify

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 // alert("API called!");
+const person = { name: "Pranta", age: 23, address: "BD" };
+
+// stringify a object
+const jsonPerson = JSON.stringify(person);
+console.log(jsonPerson);
+
+// parse to original object
+const parsePerson = JSON.parse(jsonPerson);
+console.log(parsePerson);
